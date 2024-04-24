1a5b4f43fac624de1403fde58bca0de4357dea91
Arsenii01 <100406298+Arsenii01@users.noreply.github.com>
2024-04-24 20:41:07 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -13,11 +13,13 @@ import java.util.regex.Pattern;
 public class Main {
 
     public static void main(String[] args) {
+        // Если не передан путь к файлу в командной строке, завершаем работу программы
         if (args.length == 0) {
             System.out.println("Usage: java Main <html_file>");
             return;
         }
 
+        // Получаем из аргументов путь к файлу, запускаем нашу функцию, печатаем результат в консоль
         String htmlFile = args[0];
         try {
             String result = findUniqueDomains(htmlFile);
@@ -28,12 +30,16 @@ public class Main {
     }
 
     public static String findUniqueDomains(String htmlFile) throws IOException {
+        // Получение текстового представления html-файла
         String htmlContent = Files.readString(Paths.get(htmlFile));
 
+        // Компиляция паттерна и матчинг по тексту
         Pattern linkPattern = Pattern.compile("(?:[\\p{L}\\d-]+\\.)+[\\p{L}\\d-]+");
         Matcher matcher = linkPattern.matcher(htmlContent);
 
         Set<String> uniqueDomains = new HashSet<>();
+        // Пока находятся совпадения с паттерном, добавляем их в множество
+        // Если домен начинается с 'www.', удаляем данную часть из домена
         while (matcher.find()) {
             String domain = matcher.group();
             if (domain.startsWith("www.")) {
@@ -42,9 +48,11 @@ public class Main {
             uniqueDomains.add(domain.toLowerCase());
         }
 
+        // Приводим с списку найденные домены и сортируем по условию задачи
         List<String> domainsList = new ArrayList<>(uniqueDomains);
         domainsList.sort(String::compareTo);
 
+        // Создаём JSON-строку формата, заданного в условии
         StringBuilder jsonBuilder = new StringBuilder("{\"sites\": [");
         boolean first = true;
         for (String domain : domainsList) {
