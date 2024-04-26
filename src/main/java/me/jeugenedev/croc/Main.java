fc7c31cadb4c5e5367aa1f042b7bf832cf59499f
jEUGENEdev <genyamateyuk@gmail.com>
2024-04-27 02:12:31 +0300

    implementation of the task

--- src/main/java/me/jeugenedev/croc/Main.java
+++ src/main/java/me/jeugenedev/croc/Main.java
@@ -1,17 +1,61 @@
 package me.jeugenedev.croc;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import me.jeugenedev.croc.analyzer.TextAnalyzer;
+import me.jeugenedev.croc.analyzer.impl.HtmlLinksTextAnalyzer;
+import me.jeugenedev.croc.representation.DomainManager;
+
+import java.io.*;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.util.HashSet;
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    }
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    public static String findDomainsAsString(String string, Charset cs) {
+        ByteArrayInputStream byteArray = new ByteArrayInputStream(string.getBytes(cs));
+        return findDomains(byteArray, cs);
+    }
+
+    public static String findDomainsAsFile(File file, Charset cs) throws IOException {
+        return findDomains(Files.newInputStream(file.toPath()), cs);
+    }
+
+    public static String findDomains(InputStream stream, Charset cs) {
+        Set<String> domains = new HashSet<>();
+        DomainManager domainManager = new DomainManager();
+        try (BufferedReader buf = new BufferedReader(new InputStreamReader(stream, cs))) {
+            StringBuilder bucket = new StringBuilder();
+            int countInBucket = 0, maxSize = 200;
+            String line;
+
+            while ((line = buf.readLine()) != null) {
+                bucket.append(line).append('\n');
+
+                if(countInBucket++ >= maxSize) {
+                    processBucket(bucket, domains);
+                    countInBucket = 0;
+                    bucket.setLength(0);
+                }
+            }
+            processBucket(bucket, domains);
+
+            domains.stream()
+                    .map(domain -> domain.startsWith("www.") ? domain.substring("www.".length()) : domain)
+                    .sorted()
+                    .forEach(domainManager::addDomain);
+            return domainManager.toString();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
+    }
+
+    private static void processBucket(StringBuilder bucket, Set<String> domains) {
+        if (bucket.isEmpty()) return;
+
+        TextAnalyzer textAnalyzer = new HtmlLinksTextAnalyzer(bucket.deleteCharAt(bucket.length()-1).toString());
+        textAnalyzer.findAll().stream().map(String::valueOf).forEach(domains::add);
     }
 }
