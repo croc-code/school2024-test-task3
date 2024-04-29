5c3da370509fe154425198e99c6eeca4bcb7bc29
Denisov Alexey <denisov.1881@yandex.ru>
2024-04-29 12:25:06 +0300

    Solution loaded

---
+++ UniqueDomainsExtractor/UniqueDomainsExtractorApp/DomainsJsonConverter.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace UniqueDomainsExtractorApp
+{
+	public static class DomainsJsonConverter
+	{
+		public static string ToString(string[] domains)
+		{
+			var jsonObject = new
+			{
+				sites = domains
+			};
+			return JsonConvert.SerializeObject(jsonObject, Formatting.Indented);
+		}
+	}
+}
---
+++ UniqueDomainsExtractor/UniqueDomainsExtractorApp/UniqueDomainsExtractor.cs
@@ -0,0 +1,25 @@
+﻿using HtmlAgilityPack;
+
+namespace UniqueDomainsExtractorApp
+{
+	public static class UniqueDomainsExtractor
+	{
+		public static string[] ExtractToArray(HtmlDocument doc)
+		{
+			var linkNodes = doc.DocumentNode.SelectNodes("//a[@href]");
+
+			var uniqueDomains = new HashSet<string>();
+			foreach (var node in linkNodes)
+			{
+				try
+				{
+					var uri = new Uri(node.GetAttributeValue("href", string.Empty));
+					if (uri.Host != string.Empty) { uniqueDomains.Add(uri.Host); }
+				}
+				catch (UriFormatException) { }
+			}
+
+			return uniqueDomains.ToArray();
+		}
+	}
+}
